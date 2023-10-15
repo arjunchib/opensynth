53d50216c3371c64c893a1c82626ce7dab5051ef
Arjun Chib <arjunpatrickchib@gmail.com>
2023-10-15 10:02:21 -0400

    Add asdr envelope

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'opensynth';
+  audioContext = new AudioContext();
+  oscNode = new OscillatorNode(this.audioContext, {
+    frequency: 380,
+    type: 'square',
+  });
+  gainNode = new GainNode(this.audioContext);
+  volume = 1;
+
+  ngOnInit(): void {
+    this.gainNode.gain.setValueAtTime(0, 0);
+    this.oscNode.connect(this.gainNode).connect(this.audioContext.destination);
+  }
+
+  start() {
+    this.oscNode.start();
+  }
+
+  stop() {
+    this.oscNode.stop();
+  }
+
+  onKeyDown() {
+    const attack = 0.1;
+    const decay = 0.2;
+    const sustain = 0.5;
+    const time = this.audioContext.currentTime;
+    this.gainNode.gain.linearRampToValueAtTime(this.volume, time + attack);
+    this.gainNode.gain.linearRampToValueAtTime(sustain, time + attack + decay);
+  }
+
+  onKeyUp() {
+    const release = 0.8;
+    const time = this.audioContext.currentTime;
+    this.gainNode.gain.linearRampToValueAtTime(0, time + release);
+  }
 }
