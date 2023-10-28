7ab9bb22bc856127b07afba0beb794ee70a5c5e9
Arjun Chib <arjunpatrickchib@gmail.com>
2023-10-28 15:56:53 -0400

    Improve hover effect

--- src/app/draggable/draggable.directive.ts
+++ src/app/draggable/draggable.directive.ts
@@ -14,7 +14,7 @@ import { ContextService } from '../context/context.service';
 export class DraggableDirective {
   @HostBinding('class.absolute') absolute = true;
   @HostBinding('class.select-none') selectNone = true;
-  @HostBinding('class.shadow-xl') shadow = false;
+  @HostBinding('class') class: string[] = [];
   @HostBinding('style.left.px') left = this.el.nativeElement.clientLeft;
   @HostBinding('style.top.px') top = this.el.nativeElement.clientTop;
   dragging?: [number, number];
@@ -26,12 +26,12 @@ export class DraggableDirective {
   @HostListener('mousedown', ['$event']) onMouseDown(e: MouseEvent) {
     if (this.contextService.mode.value !== 'normal') return;
     this.dragging = [e.clientX - this.left, e.clientY - this.top];
-    this.shadow = true;
+    this.class = ['shadow-xl', 'transition-dragging', '-translate-y-0.5'];
   }
 
   @HostListener('mouseup', ['$event']) onMouseUp(e: MouseEvent) {
     this.dragging = undefined;
-    this.shadow = false;
+    this.class = [];
   }
 
   @HostListener('window:mousemove', ['$event']) onMouseMove(e: MouseEvent) {
